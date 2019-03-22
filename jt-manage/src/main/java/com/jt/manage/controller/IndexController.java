86fac4b012bc594c5d72f74e20cd99bc6680b568
lipdabaichi <605576222@qq.com>
2019-03-22 21:49:53 +0800

    加上了jt-bilibili前台骨架

--- jt-manage/src/main/java/com/jt/manage/controller/IndexController.java
+++ jt-manage/src/main/java/com/jt/manage/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class IndexController {
 	
 	//实现页面跳转    采用restFul结构接收参数
