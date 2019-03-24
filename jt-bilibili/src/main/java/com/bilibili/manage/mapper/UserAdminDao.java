43c9dd0039011e0b398f3571454232cde03561fd
lipdabaichi <605576222@qq.com>
2019-03-24 16:10:44 +0800

    保存密码还有点小bug

---
+++ jt-bilibili/src/main/java/com/bilibili/manage/mapper/UserAdminDao.java
@@ -0,0 +1,10 @@
+package com.bilibili.manage.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface UserAdminDao {
+    int updatePassword(
+            @Param("username")String username,
+            @Param("newPwd")String newPwd,
+            @Param("salt")String salt);
+}
